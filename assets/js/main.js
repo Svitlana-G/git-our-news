ca95c71ce39f06ed7518fa5bebcacb18c55648a3
Isabelle-github <ixavierayix@yahoo.fr>
2021-06-10 15:10:21 +0200

    nothing

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,3 @@
 // IN der Box das Result zurückgeben ==> Samir
 // Key: 6198b77b7ab94542bd32134d9f46635e
 
-const myAPI = {
-    key: "6198b77b7ab94542bd32134d9f46635e",
-    url_base: "https://newsapi.org/v2/sources?apiKey=6198b77b7ab94542bd32134d9f46635e"
-}
