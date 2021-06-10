0071760e1f00d2258588ff9c0a06274be3acd558
Svitlana-G <svitlana.golubieva@gmail.com>
2021-06-10 15:08:33 +0200

    select in html add

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,19 @@ const myAPI = {
     key: "6198b77b7ab94542bd32134d9f46635e",
     url_base: "https://newsapi.org/v2/sources?apiKey=6198b77b7ab94542bd32134d9f46635e"
 }
+
+const searchbox = document.getElementById('searchbox')
+const date = document.getElementById('start')
+
+function searchArticle() {
+    console.log(searchbox.value)
+    console.log(date.value)
+}
+
+fetch('http://newsapi.org/v2/sources?apiKey=6198b77b7ab94542bd32134d9f46635e')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+// fetch('http://newsapi.org/v2/top-headlines?country=de&apiKey=6198b77b7ab94542bd32134d9f46635e')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
