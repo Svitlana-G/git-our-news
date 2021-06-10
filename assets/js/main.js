067a292b1420b47afe4bfd0fb556e01709f0cfdb
Isabelle-github <ixavierayix@yahoo.fr>
2021-06-10 14:01:05 +0200

    basic html

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Inputs abholen ==> Svitlana
+// API Fetch function
+// IN der Box das Result zurückgeben ==> Samir
+// Key: 6198b77b7ab94542bd32134d9f46635e
+
+const myAPI = {
+    key: "6198b77b7ab94542bd32134d9f46635e",
+    url_base: "https://newsapi.org/v2/sources?apiKey=6198b77b7ab94542bd32134d9f46635e"
+}
