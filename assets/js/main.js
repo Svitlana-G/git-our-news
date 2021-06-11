56d4e97f350c1e55e8de39ab0448e39ac9673152
Isabelle-github <ixavierayix@yahoo.fr>
2021-06-11 11:32:56 +0200

    date input edited,onload event

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
 // Inputs abholen ==> Svitlana
 // API Fetch function
 // IN der Box das Result zurückgeben ==> Samir
-// Key: 6198b77b7ab94542bd32134d9f46635e
+// ISabelleKey: 6198b77b7ab94542bd32134d9f46635e
+// SamirKey: 9e340e4b19664220be3bd0e04a3bbb15
+//SvitlanaKey: 94798b1296374af2bf61c5454a75c435
+//PräsiKey: 
 
 const myAPI = {
     key: "6198b77b7ab94542bd32134d9f46635e",
     url_base: "https://newsapi.org/v2/sources?apiKey=6198b77b7ab94542bd32134d9f46635e"
 }
 
-
+window.onload = (event) => {
+    console.log('page is fully loaded');
+    placeholder();
+};
 const searchbox = document.getElementById('searchbox')
 const date = document.getElementById('start')
 const result = document.getElementById('result')
@@ -17,7 +23,7 @@ function placeholder() {
     let today = new Date().toISOString().slice(0, 10)
     let arrayTopics = ['Sport', 'Bussiness', 'Politics', 'Cinema', 'Health', 'Entertainment', 'Science', 'Technology', 'Apple', 'USA']
     let randomTopic = arrayTopics[Math.floor(Math.random() * arrayTopics.length)];
-    fetch(`http://newsapi.org/v2/everything?q=${randomTopic}&from=${today}&sortBy=popularity&apiKey=6198b77b7ab94542bd32134d9f46635e`)
+    fetch(`http://newsapi.org/v2/everything?q=${randomTopic}&from=${today}&sortBy=popularity&apiKey=9e340e4b19664220be3bd0e04a3bbb15`)
         .then(response => response.json())
         .then(json => {
             console.log(json.articles)
@@ -31,14 +37,13 @@ function placeholder() {
             <p>${elem.description} <a href="${elem.url}">weiter lesen</a> </p> </article>`
             })
         })
-
 }
-placeholder();
+
 function searchArticle() {
     console.log(searchbox.value)
     console.log(date.value)
     result.innerHTML = ''
-    fetch(`http://newsapi.org/v2/everything?q=${searchbox.value}&from=${date.value}&sortBy=popularity&apiKey=6198b77b7ab94542bd32134d9f46635e`)
+    fetch(`http://newsapi.org/v2/everything?q=${searchbox.value}&from=${date.value}&sortBy=popularity&apiKey=9e340e4b19664220be3bd0e04a3bbb15`)
         .then(response => response.json())
         .then(json => {
             console.log(json.articles)
