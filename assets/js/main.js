8f12a3497c73e025e1b4c4b38876a3196b15dd65
OezcanEser <oe.eser84@gmail.com>
2021-06-11 11:23:00 +0200

    lilbreak

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,9 @@
 // Key: 6198b77b7ab94542bd32134d9f46635e
 
 const myAPI = {
-    key: "6198b77b7ab94542bd32134d9f46635e",
-    url_base: "https://newsapi.org/v2/sources?apiKey=6198b77b7ab94542bd32134d9f46635e"
+    key: "802107b8baf845b1b7dd21af9f0439e2",
+    url_base: "https://newsapi.org/v2/sources?apiKey=802107b8baf845b1b7dd21af9f0439e2"
 }
-
 
 const searchbox = document.getElementById('searchbox')
 const date = document.getElementById('start')
@@ -17,7 +16,7 @@ function placeholder() {
     let today = new Date().toISOString().slice(0, 10)
     let arrayTopics = ['Sport', 'Bussiness', 'Politics', 'Cinema', 'Health', 'Entertainment', 'Science', 'Technology', 'Apple', 'USA']
     let randomTopic = arrayTopics[Math.floor(Math.random() * arrayTopics.length)];
-    fetch(`http://newsapi.org/v2/everything?q=${randomTopic}&from=${today}&sortBy=popularity&apiKey=6198b77b7ab94542bd32134d9f46635e`)
+    fetch(`http://newsapi.org/v2/everything?q=${randomTopic}&from=${today}&sortBy=popularity&apiKey=802107b8baf845b1b7dd21af9f0439e2`)
         .then(response => response.json())
         .then(json => {
             console.log(json.articles)
@@ -38,19 +37,18 @@ function searchArticle() {
     console.log(searchbox.value)
     console.log(date.value)
     result.innerHTML = ''
-    fetch(`http://newsapi.org/v2/everything?q=${searchbox.value}&from=${date.value}&sortBy=popularity&apiKey=6198b77b7ab94542bd32134d9f46635e`)
+    fetch(`http://newsapi.org/v2/everything?q=${searchbox.value}&from=${date.value}&sortBy=popularity&apiKey=802107b8baf845b1b7dd21af9f0439e2`)
         .then(response => response.json())
         .then(json => {
             console.log(json.articles)
             json.articles.forEach(elem => {
                 // console.log(elem)
                 result.innerHTML += `<article>
-                <h3>You searched for News about: ${searchbox.value}</h3>
                 <h2>${elem.title}</h2>
                 <img src="${elem.urlToImage ? elem.urlToImage : ''}" alt="">
+                <h3>You searched for News about: ${searchbox.value}</h3>
                 <p>${elem.author}</p>
                 <p>${elem.description} <a href="${elem.url}">weiter lesen</a> </p> </article>`
             })
         })
 }
-
